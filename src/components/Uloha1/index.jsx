4c4aceaaa963cdf2873d9c04ee244a0856a8d9d5
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-02 20:29:02 +0200

    uloha1

--- src/components/Uloha1/index.jsx
+++ src/components/Uloha1/index.jsx
@@ -1,7 +1,15 @@
 /*
 Zadání: Po kliku na tlačítko zobrazte alert s textem „Ahoj“.
 */
+import './index.css';
 
 export const Uloha1 = () => {
-  return <button>Ukaž bublinu</button>;
+  const handleClick = () => {
+    alert('Ahoj!');
+  };
+  return (
+    <button className="greet" onClick={handleClick}>
+      Ukaž bublinu
+    </button>
+  );
 };
